b5d0477f0a5cd433be87dea132b4ffa8044f5006
jaxlinksync <jax@linksync.com>
2017-10-30 13:35:05 +0800

    fix typo for cd ..

--- init.sh
+++ init.sh
@@ -28,5 +28,5 @@ cd wav_files
 wget https://linksync-2032.kxcdn.com/wp-content/uploads/2017/06/female-voice-1.zip
 unzip female-voice-1.zip
 rm -rf female-voice-1.zip
-cd..
+cd ..
 python preprocess.py
